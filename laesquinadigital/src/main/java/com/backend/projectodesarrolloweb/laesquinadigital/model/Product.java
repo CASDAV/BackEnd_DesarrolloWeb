de160c0039580d7410dd414007723708bf47b4b8
David Alejandro Castillo Chiquiza <castillo_da@javeriana.edu.co>
2021-10-16 16:28:07 -0500

    v2

--- laesquinadigital/src/main/java/com/backend/projectodesarrolloweb/laesquinadigital/model/Product.java
+++ laesquinadigital/src/main/java/com/backend/projectodesarrolloweb/laesquinadigital/model/Product.java
@@ -40,6 +40,54 @@ public class Product {
         this.image = image;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public PurchaseOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(PurchaseOrder order) {
+        this.order = order;
+    }
+
     
 
 }
