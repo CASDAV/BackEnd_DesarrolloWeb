6fee663b90fa6753fbffd55ba78caee642a16726
David Alejandro Castillo Chiquiza <castillo_da@javeriana.edu.co>
2021-11-18 16:22:23 -0500

    Servicios agregados

---
+++ laesquinadigital/src/main/java/com/backend/projectodesarrolloweb/laesquinadigital/service/IShoppingCartService.java
@@ -0,0 +1,19 @@
+package com.backend.projectodesarrolloweb.laesquinadigital.service;
+
+import com.backend.projectodesarrolloweb.laesquinadigital.model.ShoppingCart;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IShoppingCartService {
+
+    void deleteShoppingCart(Long id);
+
+    ShoppingCart updateShoppingCart(ShoppingCart cart, Long id);
+
+    ShoppingCart getShoppingCartById(Long id);
+
+    ShoppingCart createShoppingCart(ShoppingCart shoppingCart);
+    
+    Page<ShoppingCart> getCarts(Pageable pageable);
+}
